795ee134d3b264027d817c22490c92c5d0307bd6
Wallace Su <wallacesu2015@gmail.com>
2025-03-31 12:18:48 -0400

    unique random key gen for pdfs

--- neuronote_study/api/models.py
+++ neuronote_study/api/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+import random
+import string
+
+def generateRandomKey():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=16))
 
 class uploadPDF(models.Model):
     pdf_file = models.FileField(upload_to='pdf_files/')
-    pdf_key = models.CharField(max_length=255)
+    pdf_key = models.CharField(max_length=255, default=generateRandomKey, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
