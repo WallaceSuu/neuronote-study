8023095a68a3103e48ba241ead5815a581dce7e9
WallaceSuu <72478639+WallaceSuu@users.noreply.github.com>
2025-05-26 16:58:52 -0400

    frontend deployment
    - changing configs, adding necessary things for frontend deployment (will do a decoupled deployment)

--- neuronote_study/frontend/webpack.config.js
+++ neuronote_study/frontend/webpack.config.js
@@ -1,35 +1,45 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "./static/frontend"),
-    filename: "[name].js",
-  },
-  watch: true,
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, isProduction ? "./build" : "./static/frontend"),
+      filename: isProduction ? "[name].[contenthash].js" : "[name].js",
+      publicPath: isProduction ? '/' : undefined,
+    },
+    watch: !isProduction,
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
         },
-      },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    optimization: {
+      minimize: isProduction,
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env": {
+          NODE_ENV: JSON.stringify(isProduction ? "production" : "development"),
+        },
+      }),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html',
+      }),
     ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  optimization: {
-    minimize: true,
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("development"),
-      },
-    }),
-  ],
+  };
 };
